8a7c547306c71d3f8602779ed957999a65100539
Kunt Betilmerzaev <kunt.betilmerzaev@mail.ru>
2025-01-21 14:02:22 +0300

    js1

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -11,11 +11,14 @@ let dice1 = Math.floor(Math.random() * 6)
 let dice2 = Math.floor(Math.random() * 6)
 let isWinningDouble // your code
 
-
+alert('На первом кубике выпало: ' + (dice1 + 1))
+alert('На втором кубике выпало: ' + (dice2 + 1))
 
 if (dice1 === dice2 && (+dice1 + 1) > 3 && (+dice2 + 1) > 3) {
+    alert('You win')
     isWinningDouble = true
 } else {
+    alert('You lose')
     isWinningDouble = false
 }
 
--- main.js
+++ main.js
@@ -2,14 +2,15 @@ let dice1 = Math.floor(Math.random() * 6)
 let dice2 = Math.floor(Math.random() * 6)
 let isWinningDouble // your code
 
-alert('На первом кубике выпало: ' + (+dice1 + 1))
-alert('На втором кубике выпало: ' + (+dice2 + 1))
+alert('На первом кубике выпало: ' + (dice1 + 1))
+alert('На втором кубике выпало: ' + (dice2 + 1))
 
 if (dice1 === dice2 && (+dice1 + 1) > 3 && (+dice2 + 1) > 3) {
-    alert ('You win')
+    alert('You win')
     isWinningDouble = true
 } else {
     alert('You lose')
+    isWinningDouble = false
 }
 
 
