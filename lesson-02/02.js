20bd7dfc1596dc891e682243d7f3bff0bbc89477
Kunt Betilmerzaev <kunt.betilmerzaev@mail.ru>
2025-01-22 02:13:44 +0300

    js1

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -11,14 +11,14 @@ let dice1 = Math.floor(Math.random() * 6)
 let dice2 = Math.floor(Math.random() * 6)
 let isWinningDouble // your code
 
-alert('На первом кубике выпало: ' + (dice1 + 1))
-alert('На втором кубике выпало: ' + (dice2 + 1))
+console.log('На первом кубике выпало: ' + (dice1 + 1))
+console.log('На втором кубике выпало: ' + (dice2 + 1))
 
 if (dice1 === dice2 && (+dice1 + 1) > 3 && (+dice2 + 1) > 3) {
-    alert('You win')
+    console.log('You win');
     isWinningDouble = true
 } else {
-    alert('You lose')
+    console.log('You lose')
     isWinningDouble = false
 }
 
