92a27ae48d454807d8857c353999c7423e11a105
Kunt Betilmerzaev <kunt.betilmerzaev@mail.ru>
2025-02-07 23:09:02 +0300

    js1

--- lesson-05/01.js
+++ lesson-05/01.js
@@ -16,23 +16,16 @@ const game = {
     lumber: 100,
   },
   addResource(resource, amount) {
-    
-
-      if (resource === this.resources) {
-        this.resources[resource] += amount
-        console.log("New number: ", `${this.resources[resource]}`);
-        
-      } else {
-        console.log("Invalid resource");
-        
-      }
-
-      
-    
+    if (this.resources[resource]) {
+      this.resources[resource] += amount;
+      console.log("New number: ", this.resources[resource]);
+    } else {
+      console.log("Invalid resource");
+    }
   }
-}
+};
 
-game.addResource(gold, 100)
+game.addResource('gold', 100);
 
 
 
